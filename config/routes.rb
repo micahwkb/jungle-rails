7789f9e37890e0fa96c4eb6bd2a7e9e084704487
Micah Britton <micahwkb@gmail.com>
2016-12-08 13:38:51 -0500

    built up basic login/logout and session-tracking functions

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,16 @@ Rails.application.routes.draw do
     resources :categories, except: [:edit, :update, :show]
   end
 
+  # ----  NEW USER ROUTES  ----
+  get '/register' => 'users#new'
+  post '/users'   => 'users#create'
+
+  # ----  SESSION ROUTES  ----
+  get '/login'    => 'sessions#new'
+  post '/login'   => 'sessions#create'
+  get '/logout'   => 'sessions#destroy'
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
