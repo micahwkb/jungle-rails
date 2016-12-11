6845ac88751d0cb294a054ff459d8c5cd084d862
Micah Britton <micahwkb@gmail.com>
2016-12-11 11:48:00 -0500

    updated login page to display meaningful errors on failure

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -8,10 +8,10 @@ class RatingsController < ApplicationController
                          product_id: params[:product_id],
                          rating: params[:rating][:rating])
     if @rating.save
-      redirect_to "/products/#{params[:product_id]}",
+      redirect_to product_path(params[:product_id]),
                   notice: 'Thanks for your review'
     else
-      redirect_to "/products/#{params[:product_id]}",
+      redirect_to product_path(params[:product_id]),
                   notice: 'Unable to save your review - was the description empty?'
     end
   end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,19 +10,23 @@ class SessionsController < ApplicationController
       session[:user_id] = user.id
       # redirect based on admin status
       if user.admin?
-        redirect_to '/admin'
+        redirect_to admin_root_path
       else
-        redirect_to '/'
+        redirect_to root_path
       end
 
     else
-      redirect_to '/login'
+      if !user
+        redirect_to login_path, notice: 'Invalid email'
+      else
+        redirect_to login_path, notice: 'Invalid password'
+      end
     end
   end
 
   def destroy
     session[:user_id] = nil
-    redirect_to '/'
+    redirect_to root_path
   end
 
 end
