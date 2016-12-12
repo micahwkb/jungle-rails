70ee92694393ea95497d3c49e3502a8fac84beef
Micah Britton <micahwkb@gmail.com>
2016-12-12 14:58:52 -0500

    built most of the user_spec test, adjusted users_controller to apply downcase to email before creating new user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,7 @@ class UsersController < ApplicationController
 
   def create
     user = User.new(user_params)
+    user.email.downcase!
     if user.save
       session[:user_id] = user.id
       redirect_to root_path
@@ -16,7 +17,13 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+    params.require(:user).permit(
+      :first_name,
+      :last_name,
+      :email,
+      :password,
+      :password_confirmation
+      )
   end
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,15 +1,87 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it "has a valid factory" do
-    user = build(:user)
-    expect(user).to be_valid
+  before(:each) do
+    @password = Faker::Internet.password
+    @user = User.new(
+      first_name: Faker::Name.first_name,
+      last_name:  Faker::Name.last_name,
+      email:      Faker::Internet.email,
+      password:   @password,
+      password_confirmation: @password
+      )
+    @errors = @user.errors.full_messages[0]
+  end
+
+  puts 'User model tests running...'
+
+  it 'successfully creates a valid user' do
+    @user.save
+    expect(@user).to be_valid
+  end
+
+  it 'fails to save user if passwords do not match' do
+    @user.password = Faker::Internet.password
+    @user.save
+    expect(@user.errors.full_messages[0]).to eq("Password confirmation doesn't match Password")
+  end
+
+  it 'fails to save user if the password field is nil' do
+    @user.password = nil
+    @user.save
+    expect(@user.errors.full_messages[0]).to eq("Password can't be blank")
+  end
+
+  it 'fails to save user if first_name is nil' do
+    @user.first_name = nil
+    @user.save
+    expect(@user.errors.full_messages[0]).to eq("First name can't be blank")
+  end
+
+  it 'fails to save user if last_name is nil' do
+    @user.last_name = nil
+    @user.save
+    expect(@user.errors.full_messages[0]).to eq("Last name can't be blank")
+  end
+
+  it 'fails to save user if email is nil' do
+    @user.email = nil
+    @user.save
+    expect(@user.errors.full_messages[0]).to eq("Email can't be blank")
+  end
+
+  it 'does not allow duplicate user email' do
+    @user.email = 'test@test.com'
+    @user.save
+    duplicate = User.create(
+      first_name: Faker::Name.first_name,
+      last_name:  Faker::Name.last_name,
+      email:      'test@test.com',
+      password:   @password,
+      password_confirmation: @password
+      )
+    expect(duplicate.errors.full_messages[0]).to eq("Email has already been taken")
+  end
+
+  it 'is not case-sensitive to email duplicates' do
+    @user.email = 'test@test.com'
+    @user.save
+    duplicate = User.create(
+      first_name: Faker::Name.first_name,
+      last_name:  Faker::Name.last_name,
+      email:      'TEST@test.COM',
+      password:   @password,
+      password_confirmation: @password
+      )
+    expect(duplicate.errors.full_messages[0]).to eq("Email has already been taken")
   end
 end
 
-describe User do
-  it { is_expected.to validate_presence_of(:first_name) }
-  it { is_expected.to validate_presence_of(:last_name) }
-  it { is_expected.to validate_presence_of(:email) }
-  it { is_expected.to validate_uniqueness_of(:email) }
-end
+# tests below are functional - uses shoulda/matchers
+# describe User do
+#   it { is_expected.to validate_presence_of(:first_name) }
+#   it { is_expected.to validate_presence_of(:last_name) }
+#   it { is_expected.to validate_presence_of(:email) }
+#   it { is_expected.to validate_uniqueness_of(:email) }
+# end
+
